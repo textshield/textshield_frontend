923bda1a2a1d81b770a73b2d5e17e073972cccdc
textshield <131323091+textshield@users.noreply.github.com>
2023-12-09 19:13:53 -0500

    Delete frontend directory

--- frontend/src/pages/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'http://localhost:5000';
-
-export const getHello = async () => {
-    const response = await axios.get(`${API_URL}/api`);
-    return response.data;
-};
